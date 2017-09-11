c337bea09b8d257a830637c2936b5d366b9e73ed
Chayakorn Chayaphaniphatt <58160285@go.buu.ac.th>
2017-09-11 21:21:01 +0700

    Test Passed echo Love - Love

---
+++ tennis-workshop.spec.js
@@ -0,0 +1,24 @@
+function TennisGame() {
+   this.reset = () => {
+
+   }
+
+   this.echo = () => {
+     return 'Love - Love'
+   }
+
+  }
+
+ test('Test echo Love - Love when game start', () => {
+   // Arrange
+   let app = new TennisGame()
+   app.reset()
+
+   // Act
+   let result = app.echo()
+
+   // Assert
+   expect(result).toBe('Love - Love')
+
+
+ })
